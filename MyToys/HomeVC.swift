7852cb6608b388fdda0b195ff3ad54992836ff14
hamitseyrek <hamitseyrek@gmail.com>
2022-01-16 11:20:24 +0300

    add Add button and connect with segue

--- MyToys/HomeVC.swift
+++ MyToys/HomeVC.swift
@@ -8,14 +8,17 @@
 import UIKit
 
 class HomeVC: UIViewController {
-
+    
     @IBOutlet weak var toysTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
     }
-
-
+    
+    @objc func addButtonClicked (){
+        performSegue(withIdentifier: "toDetailsVCS", sender: nil)
+    }
 }
 
