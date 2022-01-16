d752fa7d46c59ec16c5d2dbe7a336d752591b550
hamitseyrek <hamitseyrek@gmail.com>
2022-01-16 11:52:28 +0300

    hide Keyboard GestureRecognizer

--- MyToys/HomeVC.swift
+++ MyToys/HomeVC.swift
@@ -14,9 +14,21 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // add Add icon
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        // hide keyboard when click in viewcontroller
+        let closeKeyboard = UIGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(closeKeyboard)
     }
     
+    //hide Keyboard
+    @objc func hideKeyboard(){
+        view.endEditing(true)
+    }
+    
+    
+    // click Add button
     @objc func addButtonClicked (){
         performSegue(withIdentifier: "toDetailsVCS", sender: nil)
     }
